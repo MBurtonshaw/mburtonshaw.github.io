a4908ec891c4d8a26bfc8bed8c57160c9398872f
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2019-12-15 18:41:54 -0800

    Update seeds.js

--- seeds.js
+++ seeds.js
@@ -3,7 +3,7 @@ const rowLength = document.getElementById("row-length");
 const plantSpacing = document.getElementById("plant-spacing");
 
 const rowRows = document.getElementById("row-rows");
-    
+
 const rows = document.getElementById("rows");
 
 const body = document.getElementsByTagName("BODY")[0];
@@ -18,12 +18,14 @@ let button2 = document.getElementById("button2");
 
 let button3 = document.getElementById("button3");
 
+let resetButton = document.getElementById("resetButton");
+
 function calculate() {
     let a = parseFloat(rowLength.value, 10);
     let b = parseFloat(plantSpacing.value, 10);
     let c = parseFloat(rows.value, 10);
     let d = parseFloat(rowRows.value, 10);
-    return Math.ceil(((((a*12)/b)*c)*d))
+    return Math.ceil(((a * 12) / b) * c * d);
 }
 
 function calculate2() {
@@ -31,7 +33,7 @@ function calculate2() {
     let b = parseFloat(plantSpacing.value, 10);
     let c = parseFloat(rows.value, 10);
     let d = parseFloat(rowRows.value, 10);
-    return Math.ceil(((((a*12)/b)*c)*d)*2);;
+    return Math.ceil(((a * 12) / b) * c * d * 2);
 }
 
 function calculate3() {
@@ -39,14 +41,14 @@ function calculate3() {
     let b = parseFloat(plantSpacing.value, 10);
     let c = parseFloat(rows.value, 10);
     let d = parseFloat(rowRows.value, 10);
-    return Math.ceil(((((a*12)/b)*c)*d)*3);
+    return Math.ceil(((a * 12) / b) * c * d * 3);
 }
 
-    answerDiv.style.visibility = "hidden";
-    div2.style.visibility = "hidden";
-    div3.style.visibility = "hidden";
-    
-    button1.addEventListener ("click", (e) => {
+answerDiv.style.visibility = "hidden";
+div2.style.visibility = "hidden";
+div3.style.visibility = "hidden";
+
+button1.addEventListener("click", e => {
     if (answerDiv !== null) {
         answerDiv.innerHTML = "";
     }
@@ -57,19 +59,18 @@ function calculate3() {
     answerDiv.innerHTML += "<p>" + calculate() + " seeds" + "</p>";
 });
 
-    button2.addEventListener ("click", (e) => {
+button2.addEventListener("click", e => {
     if (div2 !== null) {
         div2.innerHTML = "";
     }
     button2 = e.target;
     div2.innerHTML = "<p>" + calculate2() + " seeds" + "</p>";
+});
 
-    });
-
-    button3.addEventListener ("click", (e) => {
+button3.addEventListener("click", e => {
     if (div3 !== null) {
         div3.innerHTML = "";
     }
     button3 = e.target;
     div3.innerHTML = "<p>" + calculate3() + " seeds" + "</p>";
-    });
+});
