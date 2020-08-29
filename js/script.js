2d200d19aa246f28509e35f00e3bb0e40f841d1c
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-08-29 12:12:25 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,22 @@
-if (screen.width > 1366) {
-    equation();
-}
-
-function equation() {
-    const x = $(window).width();
-    const y = x - 1349;
-    const z = y / 2;
-    $("#container").css("margin-left", z + "px");
-    $("#container").css("margin-right", z + "px");
-}
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
-
 function myFn() {
     if (clock >= 6 && clock <= 10) {
         $("#three_img").attr("src", "");
-        $("#three_img").attr("src", "img/GalleryImage_1.jpg");  
-        };
-        }
+        $("#three_img").attr("src", "img/GalleryImage_1.jpg");
+    }
+}
 
 function myFn2() {
     if (clock >= 11 && clock <= 15) {
         $("#three_img").attr("src", "");
-        $("#three_img").attr("src", "img/MenuImage_1.jpg");  
-        };
-        }
+        $("#three_img").attr("src", "img/MenuImage_1.jpg");
+    }
+}
 function myFn3() {
     if (clock >= 1 && clock <= 5) {
         $("#three_img").attr("src", "");
-        $("#three_img").attr("src", "img/HeaderImage_1.jpg");  
-        };
-};
-
+        $("#three_img").attr("src", "img/HeaderImage_1.jpg");
+    }
+}
 
 let clock = 0;
 
@@ -44,10 +25,9 @@ function timer() {
     console.log(clock);
     if (clock === 15) {
         clock = -1;
-        clock +=1;
+        clock += 1;
     }
 }
-
 
 setInterval(timer, 1000);
 
@@ -56,14 +36,12 @@ setInterval(myFn2, 1000);
 setInterval(myFn3, 1000);
 
 for (let i = 0; i < 1; i++) {
-
-    
     if (clock === 15) {
         clearInterval(myFn);
         clearInterval(myFn2);
         clearInterval(myFn3);
     }
-        setInterval(myFn, 5000);
-        setInterval(myFn2, 5000);
-        setInterval(myFn3, 5000);
+    setInterval(myFn, 5000);
+    setInterval(myFn2, 5000);
+    setInterval(myFn3, 5000);
 }
