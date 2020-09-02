b3caec375bef94fb3fb8f1216bfdeec7b400c3ed
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-09-02 04:56:38 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 function myFn() {
-    if (clock >= 6 && clock <= 10) {
+    if (clock >= 6 && clock <= 11) {
         $("#three_img").attr("src", "img/slideshow_2.png");
     }
 }
 
 function myFn2() {
-    if (clock >= 11 && clock <= 15) {
+    if (clock >= 12 && clock <= 17) {
         $("#three_img").attr("src", "img/slideshow_1.png");
     }
 }
 
 function myFn4() {
-    if (clock >= 16 && clock <= 21) {
+    if (clock >= 18 && clock <= 23) {
         $("#three_img").attr("src", "img/slideshow_3.png");
     }
 }
 
 function myFn5() {
-    if (clock >= 22 && clock <= 25) {
+    if (clock >= 24 && clock <= 29) {
         $("#three_img").attr("src", "img/slideshow_4.png");
     }
 }
 
 function myFn3() {
-    if (clock >= 1 && clock <= 5) {
+    if (clock >= 0 && clock <= 5) {
         $("#three_img").attr("src", "img/HeaderImage_2.jpg");
     }
 }
@@ -32,7 +32,7 @@ let clock = 0;
 console.log(window.innerWidth);
 function timer() {
     clock += 1;
-    if (clock === 26) {
+    if (clock === 30) {
         clock = -1;
         clock += 1;
     }
@@ -41,20 +41,23 @@ function timer() {
 
 setInterval(timer, 1000);
 
-setInterval(myFn, 1500);
-setInterval(myFn2, 1500);
-setInterval(myFn3, 1500);
-setInterval(myFn4, 500); //takes longer to load
-setInterval(myFn5, 1500);
+setInterval(myFn, 1000);
+setInterval(myFn2, 1000);
+setInterval(myFn3, 1000);
+setInterval(myFn4, 1000);
+setInterval(myFn5, 1000);
 
 for (let i = 0; i < 1; i++) {
-    if (clock === 20) {
+    if (clock === 30) {
+        clearInterval(timer);
         clearInterval(myFn);
         clearInterval(myFn2);
         clearInterval(myFn3);
         clearInterval(myFn4);
         clearInterval(myFn5);
+        setInterval(timer, 1000);
     }
+
     setInterval(myFn, 1000);
     setInterval(myFn2, 1000);
     setInterval(myFn3, 1000);
