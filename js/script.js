ac563b2329a1ff0ca9811c0ca4f9fec617f53be4
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2020-09-23 09:55:17 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,9 +65,14 @@ for (let i = 0; i < 1; i++) {
     setInterval(myFn5, 1000);
 }
 
-mobile();
+function desktop() {
+    $("#change_css").attr("href", "");
+    $("#change_css").attr("href", "css/style.css");
+}
 
 function mobile() {
+    $("#change_css").attr("href", "");
+    $("#change_css").attr("href", "css/mobile.css");
     $("#one_menu_div").html(
         "<a id='one_menu_a'><img src='img/mobile_menu_icon.png' id='one_menu_img'></a>"
     );
@@ -110,5 +115,39 @@ function mobile() {
             "<div class='klaviyo-form-RBSHP6'></div>" +
             "</div>"
     );
+    $("#one_menu_div").on("click", () => {
+        $("#container").addClass("clicker");
+        $("#container").html(
+            "<div id='divvest'>" +
+                "<h2 class='mobile_menu' id='mobile_menu'><a target='_blank' href='menu.html'>" +
+                "MENU" +
+                "</a></h2>" +
+                "<h2 class='mobile_menu' id='mobile_about'><a target='_blank' href='contact.html'>" +
+                "ABOUT" +
+                "</a></h2>" +
+                "<h2 class='mobile_menu' id='mobile_gallery'><a target='_blank' href='gallery.html'>" +
+                "GALLERY" +
+                "</a></h2>" +
+                "<h2 class='mobile_menu' id='mobile_contact'><a target='_blank' href='contact.html'>" +
+                "CONTACT" +
+                "</a></h2>" +
+                "</div>" +
+                "<div id='closer_div'>" +
+                "<img id='closer' src='img/mobile_x.png'>" +
+                "</div>"
+        );
+        $("#container").css("background-color", "rgb(173, 129, 86");
+        $("body").css("background-color", "rgb(173, 129, 86)");
+
+        $("#closer_div").on("click", () => {
+            location.reload();
+        });
+    });
 }
 
+if (window.innerWidth > 1200) {
+    desktop();
+}
+if (window.innerWidth < 1200) {
+    mobile();
+}
